f8eee889d9d1ed0f975873d8db7ae5affe46d6ac
mikaelsfm <mikael.schwind-terceiro@cea.com.br>
2025-11-30 23:51:23 -0300

    remove 25 code smells de NotaFiscalItemImpostoService

---
+++ src/main/java/net/originmobi/pdv/dto/ImpostoDTO.java
@@ -0,0 +1,179 @@
+package net.originmobi.pdv.dto;
+
+public class ImpostoDTO {
+
+    private Long codImposto;
+    private char origin;
+    private String cstCofins;
+    private int modBcIcms;
+    private Double bcIcms;
+    private Double aliqIcms;
+    private Double vlIcms;
+    private String cstPis;
+    private Double bcPis;
+    private Double pis;
+    private Double vlPis;
+    private Double bcCofins;
+    private Double aliqCofins;
+    private Double vlCofins;
+    private int cst;
+    private int cstIpi;
+    private Double vbcIpi;
+    private Double pIpi;
+    private Double vIpi;
+
+    public ImpostoDTO() {
+    }
+
+    public Long getCodImposto() {
+        return codImposto;
+    }
+
+    public void setCodImposto(Long codImposto) {
+        this.codImposto = codImposto;
+    }
+
+    public char getOrigin() {
+        return origin;
+    }
+
+    public void setOrigin(char origin) {
+        this.origin = origin;
+    }
+
+    public String getCstCofins() {
+        return cstCofins;
+    }
+
+    public void setCstCofins(String cstCofins) {
+        this.cstCofins = cstCofins;
+    }
+
+    public int getModBcIcms() {
+        return modBcIcms;
+    }
+
+    public void setModBcIcms(int modBcIcms) {
+        this.modBcIcms = modBcIcms;
+    }
+
+    public Double getBcIcms() {
+        return bcIcms;
+    }
+
+    public void setBcIcms(Double bcIcms) {
+        this.bcIcms = bcIcms;
+    }
+
+    public Double getAliqIcms() {
+        return aliqIcms;
+    }
+
+    public void setAliqIcms(Double aliqIcms) {
+        this.aliqIcms = aliqIcms;
+    }
+
+    public Double getVlIcms() {
+        return vlIcms;
+    }
+
+    public void setVlIcms(Double vlIcms) {
+        this.vlIcms = vlIcms;
+    }
+
+    public String getCstPis() {
+        return cstPis;
+    }
+
+    public void setCstPis(String cstPis) {
+        this.cstPis = cstPis;
+    }
+
+    public Double getBcPis() {
+        return bcPis;
+    }
+
+    public void setBcPis(Double bcPis) {
+        this.bcPis = bcPis;
+    }
+
+    public Double getPis() {
+        return pis;
+    }
+
+    public void setPis(Double pis) {
+        this.pis = pis;
+    }
+
+    public Double getVlPis() {
+        return vlPis;
+    }
+
+    public void setVlPis(Double vlPis) {
+        this.vlPis = vlPis;
+    }
+
+    public Double getBcCofins() {
+        return bcCofins;
+    }
+
+    public void setBcCofins(Double bcCofins) {
+        this.bcCofins = bcCofins;
+    }
+
+    public Double getAliqCofins() {
+        return aliqCofins;
+    }
+
+    public void setAliqCofins(Double aliqCofins) {
+        this.aliqCofins = aliqCofins;
+    }
+
+    public Double getVlCofins() {
+        return vlCofins;
+    }
+
+    public void setVlCofins(Double vlCofins) {
+        this.vlCofins = vlCofins;
+    }
+
+    public int getCst() {
+        return cst;
+    }
+
+    public void setCst(int cst) {
+        this.cst = cst;
+    }
+
+    public int getCstIpi() {
+        return cstIpi;
+    }
+
+    public void setCstIpi(int cstIpi) {
+        this.cstIpi = cstIpi;
+    }
+
+    public Double getVbcIpi() {
+        return vbcIpi;
+    }
+
+    public void setVbcIpi(Double vbcIpi) {
+        this.vbcIpi = vbcIpi;
+    }
+
+    public Double getpIpi() {
+        return pIpi;
+    }
+
+    public void setpIpi(Double pIpi) {
+        this.pIpi = pIpi;
+    }
+
+    public Double getvIpi() {
+        return vIpi;
+    }
+
+    public void setvIpi(Double vIpi) {
+        this.vIpi = vIpi;
+    }
+}
